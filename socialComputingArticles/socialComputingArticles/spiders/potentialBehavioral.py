33dc486c8ec3355b092abb0d8ffd35daf347b25e
Nalini Dutt <72424386+nalinidutt@users.noreply.github.com>
2021-06-09 15:50:24 -0400

    added summarization

--- socialComputingArticles/socialComputingArticles/spiders/potentialBehavioral.py
+++ socialComputingArticles/socialComputingArticles/spiders/potentialBehavioral.py
@@ -1,14 +1,22 @@
 import scrapy
 import sumy
 
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+
+summarizer = LexRankSummarizer()
+
 class SocialcomputingarticlesItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     title = scrapy.Field()
     intro = scrapy.Field()
+    finalSummary = scrapy.Field()
 
-class PotentialbehavioralSpider(scrapy.Spider):
-    name = 'potentialBehavioral'
+class PotentialbehaviorSummSpider(scrapy.Spider):
+    
+    name = 'potentialBehavior_summ'
     allowed_domains = ['behavioralscientist.org']
     
     def start_requests(self):
@@ -18,10 +26,19 @@ class PotentialbehavioralSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse_url)
 
     def parse_url(self, response):
+        finalSummary = ''
+        
         title = response.xpath('//*[@id="post-16220"]/header/h1/text()').get()
         intro = response.xpath('//*[@id="post-16220"]/div[2]/blockquote[1]/p/strong/text()').get()
+        
+        textParser = PlaintextParser.from_string(intro, Tokenizer('english'))
+        summary = summarizer(textParser.document, 2)
+        
+        for sentence in summary:
+            finalSummary += str(sentence)
         
         item = SocialcomputingarticlesItem()
         item["title"] = title
         item["intro"] = intro
+        item["finalSummary"] = finalSummary
         yield item
