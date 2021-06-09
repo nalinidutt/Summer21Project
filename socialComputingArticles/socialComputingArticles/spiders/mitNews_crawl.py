d38c2eb5d85b6106725a770ddeaf20196b9d2b91
Nalini Dutt <72424386+nalinidutt@users.noreply.github.com>
2021-06-09 12:16:37 -0400

    added xpaths

--- socialComputingArticles/socialComputingArticles/spiders/mitNews_crawl.py
+++ socialComputingArticles/socialComputingArticles/spiders/mitNews_crawl.py
@@ -5,6 +5,7 @@ from scrapy.spiders import CrawlSpider, Rule
 class SocialcomputingarticlesItem(scrapy.Item):
     title = scrapy.Field()
     summary = scrapy.Field()
+    headings = scrapy.Field()
 
 class mitNewsCrawlSpider(CrawlSpider):
     name = 'mitNews_crawl'
@@ -14,12 +15,12 @@ class mitNewsCrawlSpider(CrawlSpider):
     rules = (Rule(LinkExtractor(allow=r'/'), callback='parse_item', follow=True)),
 
     def parse_item(self, response):
-      # add XPaths
-        if response.xpath('').get() is not None:
-            title = response.xpath('').get()
-            summary = response.xpath('').get()
+        if response.xpath('//*[@id="block-mit-content"]/div/article/div/div[1]/span/text()').get() is not None:
+            title = response.xpath('//*[@id="block-mit-page-title"]/div/h1/span/text()').get()
+            summary = response.xpath('//*[@id="block-mit-content"]/div/article/div/div[1]/span/text()').get()
             
             item = SocialcomputingarticlesItem()
             item["title"] = title
             item["summary"] = summary
+            item["headings"] = headings
             return item
