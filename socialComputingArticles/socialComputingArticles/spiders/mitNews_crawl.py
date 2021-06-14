b8aff872835ab97c49c6db70237810bd4493ae05
Nalini Dutt <72424386+nalinidutt@users.noreply.github.com>
2021-06-14 14:25:04 -0400

    added url tracker

--- socialComputingArticles/socialComputingArticles/spiders/mitNews_crawl.py
+++ socialComputingArticles/socialComputingArticles/spiders/mitNews_crawl.py
@@ -1,5 +1,8 @@
 # OUTPUT: mitNewsInfo.csv
 
+# import csv
+import pandas as pd
+
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
@@ -13,12 +16,33 @@ from sumy.summarizers.lex_rank import LexRankSummarizer
 
 summarizer = LexRankSummarizer()
 
+colNames = ['author', 'date', 'finalSummary', 'miniSummary', 'title', 'url']
+data = pd.read_csv('mitNewsInfo.csv', names=colNames)
+
+urls = data.url.tolist()
+
+"""
+with open('../../mitNewsInfo.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    allLines = []
+    
+    for row in csv_reader:
+        for header, value in row.items():
+            try:
+                allLines[header].append(value)
+            except KeyError:
+                allLines[header] = [value]
+                
+urls = allLines['url']
+"""
+
 class SocialcomputingarticlesItem(scrapy.Item):
     title = scrapy.Field()
     miniSummary = scrapy.Field()
     author = scrapy.Field()
     date = scrapy.Field()
     finalSummary = scrapy.Field()
+    url = scrapy.Field()
 
 class mitNewsCrawlSpider(CrawlSpider):
     # variables
@@ -38,34 +62,31 @@ class mitNewsCrawlSpider(CrawlSpider):
         if self.count >= self.n:
             raise CloseSpider('all done')
         else:
-            if response.xpath('//*[@id="block-mit-content"]/div/article/div/div[1]/span/text()').get() is not None:
-                title = response.xpath('//*[@id="block-mit-page-title"]/div/h1/span/text()').get()
-                author = response.xpath('//*[@id="block-mit-content"]/div/article/div/div[3]/span[1]/text()').get()
-                date = response.xpath('//*[@id="block-mit-content"]/div/article/div/div[4]/time/text()').get()
-                miniSummary = response.xpath('//*[@id="block-mit-content"]/div/article/div/div[1]/span/text()').get()
-                
-                paragraphs = response.xpath('//*[@id="block-mit-content"]/div/article/div/div[7]/div[1]/div/div/p/text()').getall()
+            if response.url in urls:
+                if response.xpath('//*[@id="block-mit-content"]/div/article/div/div[1]/span/text()').get() is not None:
+                    title = response.xpath('//*[@id="block-mit-page-title"]/div/h1/span/text()').get()
+                    author = response.xpath('//*[@id="block-mit-content"]/div/article/div/div[3]/span[1]/text()').get()
+                    date = response.xpath('//*[@id="block-mit-content"]/div/article/div/div[4]/time/text()').get()
+                    miniSummary = response.xpath('//*[@id="block-mit-content"]/div/article/div/div[1]/span/text()').get()
 
-                for paragraph in paragraphs:
-                    fullText+= paragraph
+                    paragraphs = response.xpath('//*[@id="block-mit-content"]/div/article/div/div[7]/div[1]/div/div/p/text()').getall()
 
-                textParser = PlaintextParser.from_string(fullText, Tokenizer('english'))
-                summary = summarizer(textParser.document, 2)    # can change number of sentences
+                    for paragraph in paragraphs:
+                        fullText+= paragraph
 
-                for sentence in summary:
-                    finalSummary += str(sentence)
+                    textParser = PlaintextParser.from_string(fullText, Tokenizer('english'))
+                    summary = summarizer(textParser.document, 2)    # can change number of sentences
 
-                self.count +=1
-                
-                item = SocialcomputingarticlesItem()
-                item["title"] = title
-                item["author"] = author
-                item["date"] = date
-                item["miniSummary"] = miniSummary
-                item["finalSummary"] = finalSummary
-                return item
-            
-            
-            
-            
-            
+                    for sentence in summary:
+                        finalSummary += str(sentence)
+
+                    self.count +=1
+
+                    item = SocialcomputingarticlesItem()
+                    item["title"] = title
+                    item["author"] = author
+                    item["date"] = date
+                    item["miniSummary"] = miniSummary
+                    item["finalSummary"] = finalSummary
+                    item["url"] = response.url
+                    return item
