07ab38a69f0a50c44c3dbba2695251dc88b2ef94
Nalini Dutt <72424386+nalinidutt@users.noreply.github.com>
2021-06-24 16:50:21 -0400

    updated url tracker

--- socialComputingArticles/socialComputingArticles/spiders/mitNews_crawl.py
+++ socialComputingArticles/socialComputingArticles/spiders/mitNews_crawl.py
@@ -2,10 +2,13 @@
 
 # import pandas as pd
 import pymongo
+from scrapy.utils.project import get_project_settings
 
-client = pymongo.MongoClient()
-db = client["scrapyUrls"]
-collection = db["urls1"]
+settings = get_project_settings()
+
+connection = pymongo.MongoClient(settings.get('MONGODB_SERVER'), settings.get('MONGODB_PORT'))
+db = connection[settings.get('MONGODB_DB')]
+collection = db[settings.get('MONGODB_COLLECTION')]
 
 import scrapy
 from scrapy.linkextractors import LinkExtractor
@@ -23,7 +26,6 @@ summarizer = LexRankSummarizer()
 """
 colnames = ['author', 'date', 'finalSummary', 'miniSummary', 'title', 'url']
 data = pd.read_csv('mitNewsInfo.csv', names=colnames)
-
 urls = data.url.tolist()
 """
 
@@ -37,7 +39,7 @@ class SocialcomputingarticlesItem(scrapy.Item):
 
 class mitNewsCrawlSpider(CrawlSpider):
     # variables
-    n = 100 # number of pages
+    n = 10 # number of pages
     count = 0 # counter
     
     name = 'mitNews_crawl'
@@ -52,30 +54,31 @@ class mitNewsCrawlSpider(CrawlSpider):
         query = { "url": response.url }
         
         doc = collection.find(query)
+        list_doc = list(doc)
         
         if self.count >= self.n:
             raise CloseSpider('all done')
         else:
-            if doc is None:
+            if list_doc == []:
                 if response.xpath('//*[@id="block-mit-content"]/div/article/div/div[1]/span/text()').get() is not None:
                     title = response.xpath('//*[@id="block-mit-page-title"]/div/h1/span/text()').get()
                     author = response.xpath('//*[@id="block-mit-content"]/div/article/div/div[3]/span[1]/text()').get()
                     date = response.xpath('//*[@id="block-mit-content"]/div/article/div/div[4]/time/text()').get()
                     miniSummary = response.xpath('//*[@id="block-mit-content"]/div/article/div/div[1]/span/text()').get()
-
+    
                     paragraphs = response.xpath('//*[@id="block-mit-content"]/div/article/div/div[7]/div[1]/div/div/p/text()').getall()
-
+    
                     for paragraph in paragraphs:
                         fullText+= paragraph
-
+    
                     textParser = PlaintextParser.from_string(fullText, Tokenizer('english'))
                     summary = summarizer(textParser.document, 2)    # can change number of sentences
-
+    
                     for sentence in summary:
                         finalSummary += str(sentence)
-
+    
                     self.count +=1
-
+    
                     item = SocialcomputingarticlesItem()
                     item["title"] = title
                     item["author"] = author
