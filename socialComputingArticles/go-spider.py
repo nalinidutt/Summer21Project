5da4e81c9c19358aa6296f780ed471fa46be92e3
Nalini Dutt <72424386+nalinidutt@users.noreply.github.com>
2021-06-29 12:51:06 -0400

    Delete go-spider.py

--- socialComputingArticles/go-spider.py
+++
@@ -1,8 +0,0 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-
-from socialComputingArticles.spiders.#spider_name import # <spider_class_name>
-
-process = CrawlerProcess(get_project_settings())
-process.crawl(# <spider_class_name>)
-process.start()
