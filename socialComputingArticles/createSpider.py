ea4f9ce79be04a05f86fc5bceacc6ba5f0c6bab0
Nalini Dutt <72424386+nalinidutt@users.noreply.github.com>
2021-07-13 13:37:14 -0400

    added more vars

--- socialComputingArticles/createSpider.py
+++ socialComputingArticles/createSpider.py
@@ -6,4 +6,9 @@ template = input("Enter spider template: ")
 domain = input("Enter main domain: ")
 start_url = input("Enter start url: ")
 
+titleX = input("Enter title XPath: ")
+authorX = input("Enter author XPath: ")
+dateX = input("Enter date XPath: ")
+paragraphsX = input("Enter paragraphs XPath: ")
+
 # DOCKERFILE: scrapy genspider -t template spiderName domain
